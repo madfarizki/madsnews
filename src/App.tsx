d89517619fe6238dae8a92fe9e1b0196ae9e4724
Ahmad Alfarizki <ahmfariz24@gmail.com>
2024-05-23 13:49:03 +0700

    fix: refactor layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Footer, Header } from "@/components/index";
 import Router from "./routes/routes";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Header />
+      <Router />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,17 +1,14 @@
 import { Layout } from "antd";
-import { Footer, Header } from "@/components/index";
 import { Hero, Highlight, News } from "./sections";
 
 function Home() {
   return (
     <Layout className="bg-white">
-      <Header />
       <div className="w-full flex flex-col justify-center align-middle max-w-[1240px] mx-auto">
         <Hero />
         <Highlight />
         <News />
       </div>
-      <Footer />
     </Layout>
   );
 }
